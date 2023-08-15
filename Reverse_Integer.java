d7d9b754a88e3ac96b73804bd1aa3a89616d8746
Adarsh11Sharma <adarshdsharma.11@gmail.com>
2023-08-16 00:31:51 +0530

    Reverse Integer(leetcode)

---
+++ Reverse_Integer.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Reverse_Integer {
+
+    public static int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            // Check for overflow before updating the reversed value
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return reversed;
+    }-12
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int reversedX = reverse(x);
+        System.out.println(reversedX);
+    }
+}
