3eb6f3da1e65e858c211ba9b2edcb5f9ec191b03
Adarsh11Sharma <20102169.adarsh.sharma@gmail.com>
2025-07-27 12:05:50 +0530

    user interactive

--- IteratorLearning.java
+++ IteratorLearning.java
@@ -1,49 +1,52 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class IteratorLearning {
-  public static void main(String[] args) {
-    IteratorLearning ITR=new IteratorLearning ();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-             FirstOccuranceString finder = new FirstOccuranceString(); // create object
+        // User inputs for car brands
+        ArrayList<String> cars = new ArrayList<>();
+        System.out.print("How many car brands you want to enter? ");
+        int carCount = sc.nextInt();
+        sc.nextLine(); // consume leftover newline
 
-    // Create an ArrayList of Strings
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-    
-    ArrayList<Integer> num=new ArrayList<Integer>();
-    num.add(1);
-    num.add(2);
-    num.add(3);
-    num.add(4);
-    num.add(5);
-    Iterator<Integer> nums=num.iterator();
-    
-    while(nums.hasNext()){
-    int n=nums.next();
-    if(n==4){
-    nums.remove();
-    
+        for (int i = 0; i < carCount; i++) {
+            System.out.print("Enter car brand " + (i + 1) + ": ");
+            String brand = sc.nextLine();
+            cars.add(brand);
+        }
+
+        // User inputs for numbers
+        ArrayList<Integer> num = new ArrayList<>();
+        System.out.print("\nHow many numbers you want to enter? ");
+        int numCount = sc.nextInt();
+
+        for (int i = 0; i < numCount; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            num.add(sc.nextInt());
+        }
+
+        // Iterate through numbers and remove 4
+        System.out.println("\nNumbers (removing 4):");
+        Iterator<Integer> nums = num.iterator();
+        while (nums.hasNext()) {
+            int n = nums.next();
+            if (n == 4) {
+                nums.remove();
+            } else {
+                System.out.println(n);
+            }
+        }
+
+        // Iterate through car brands
+        System.out.println("\nCar Brands:");
+        Iterator<String> it = cars.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        sc.close(); // close the scanner
     }
-    else{System.out.println(n);
-    
-    }}
-    
-    
-    Iterator<String> It=cars.iterator();
-    
-    while(It.hasNext()){
-    System.out.println(It.next());
-}
-    // Get an iterator for the ArrayList
-    Iterator<String> it = cars.iterator();
-
-    // Iterate through the list using the iterator
-    while(it.hasNext()) {
-      System.out.println(it.next());
-    }
-  }
 }
